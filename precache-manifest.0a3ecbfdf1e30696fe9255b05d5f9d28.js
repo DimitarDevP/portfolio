675023fd93242f8970219744ca832914f17a46ed
dimitar.veljanovski@tradiebot.com <dimitarveljanovski1@gmail.com>
2020-03-25 22:16:44 +0100

    Updates

---
+++ precache-manifest.0a3ecbfdf1e30696fe9255b05d5f9d28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c0f258872849f537bede31f878d710",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2c5764e1246f55e6acde",
+    "url": "/portfolio/static/css/main.4ca55296.chunk.css"
+  },
+  {
+    "revision": "861b196b6457d94c566d",
+    "url": "/portfolio/static/js/2.72028813.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.72028813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5764e1246f55e6acde",
+    "url": "/portfolio/static/js/main.7ad84729.chunk.js"
+  },
+  {
+    "revision": "1a95985286676740b556",
+    "url": "/portfolio/static/js/runtime-main.f6eb50f0.js"
+  }
+]);
